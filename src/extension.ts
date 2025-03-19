9cd6251ddcddfe8da01932081c528c2570fc1599
martin <yang.ruoqi@outlook.com>
2025-03-19 12:16:18 +0800

    status bar item click to copy index value to clipboard

--- src/extension.ts
+++ src/extension.ts
@@ -5,8 +5,9 @@ const JSL_URL = "https://www.jisilu.cn/webapi/cb/index_quote/";
 
 export function activate(context: vscode.ExtensionContext) {
     let statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
-    statusBarItem.command = 'cbond-index.showIndex';
     context.subscriptions.push(statusBarItem);
+
+    let latestCurIndex = '';
 
     async function updateStatusBarItem() {
         try {
@@ -15,13 +16,28 @@ export function activate(context: vscode.ExtensionContext) {
             const curIndex = data.cur_index;
             const curIncreaseRt = data.cur_increase_rt;
 
-            statusBarItem.text = `C ${curIndex} ${curIncreaseRt >= 0 ? '+' : ''}${curIncreaseRt}%`;
+            latestCurIndex = curIndex.toString();
+
+            statusBarItem.text = `等权 ${curIndex} ${curIncreaseRt >= 0 ? '+' : ''}${curIncreaseRt}%`;
             statusBarItem.show();
         } catch (error) {
             statusBarItem.text = 'Failed to fetch index';
             statusBarItem.show();
         }
     }
+
+    statusBarItem.command = 'cbond-index.copyIndex';
+    statusBarItem.tooltip = 'Click to copy index';
+    statusBarItem.show();
+
+    let copyIndexDisposable = vscode.commands.registerCommand('cbond-index.copyIndex', async () => {
+        if (latestCurIndex) {
+            await vscode.env.clipboard.writeText(latestCurIndex);
+            vscode.window.showInformationMessage(`Index ${latestCurIndex} copied to clipboard.`);
+        }
+    });
+
+    context.subscriptions.push(copyIndexDisposable);
 
     let disposable = vscode.commands.registerCommand('cbond-index.showIndex', () => {
         updateStatusBarItem();
